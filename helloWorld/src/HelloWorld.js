3e5f7162b66c210965017da1dc1bac555f202edf
longyangxi <longyangxi@gmail.com>
2015-01-30 14:46:57 +0800

    Update the HelloWorld project

--- helloWorld/src/HelloWorld.js
+++ helloWorld/src/HelloWorld.js
@@ -2,8 +2,12 @@ var HelloWorld = cc.Scene.extend({
     onEnter:function(){
         this._super();
         var winSize = cc.visibleRect;
+        //从flax输出的素材文件中，创建id为anim的动画，对应flash库中链接名为mc.anim的动画
+        //添加到this中，并设置位置为舞台中心
         var anim = flax.assetsManager.createDisplay(res.anim, "anim", {parent: this, x: winSize.width/2, y: winSize.height/2});
+        //在最后一帧停住
         anim.autoStopWhenOver = true;
+        //从当前帧就是第1帧开始播放
         anim.play();
     }
 });
