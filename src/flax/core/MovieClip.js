10e6f0a3cfeb3843ec20f385a03c68be7604da57
longames0 <longames0@qq.com>
2016-01-20 17:53:56 +0800

    Fixed the memory bug of MovieClip

--- src/flax/core/MovieClip.js
+++ src/flax/core/MovieClip.js
@@ -406,6 +406,8 @@ flax._movieClip = {
     onRecycle:function()
     {
         this._super();
+        this.sameFpsForChildren = true;
+        this.createChildFromPool = true;
         this._autoPlayChildren = false;
         if(RESET_FRAME_ON_RECYCLE){
             for(var key in this.namedChildren) {
@@ -415,6 +417,14 @@ flax._movieClip = {
                 }
             }
         }
+    },
+    onExit: function () {
+        this._super();
+        for(var childName in this.namedChildren){
+            delete this.namedChildren[childName];
+            delete this[childName];
+        }
+        //this._childrenDefine = null;
     }
 };
 
